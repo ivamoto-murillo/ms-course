f1624038dfa8b5f851295bafa9257a04cb3e01a7
Murillo Ivamoto <ivamoto.murillo@gmail.com>
2021-01-06 00:18:25 -0300

    RestTemplate e Feign Client

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,82 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long              id;
+    private String            name;
+    private Double            dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return this.dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Worker other = (Worker) obj;
+
+        if (this.id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!this.id.equals(other.id)) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
